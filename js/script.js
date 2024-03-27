e0b9de667241460fa6a94d78886a1e7e8a416f99
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-03-27 21:30:04 +0100

    add: h1 con scritta FizzBuzz

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-const square = document.querySelector(".container"); // object | null
-console.log(square);
+const container = document.querySelector(".container"); // object | null
+console.log(container);
 for (let i = 1; i <= 100; i++) { //creazione di 100 numeri da 1 a 100.
    let number = i;
-   let myClassAdd = "blu";
+   let myClassAdd = "blu"; // Imposto tutti i quadrati di colore blu.
     if (i % 3 === 0 && i % 5 === 0) {
         number = "FizzBuzz"; //Sostituisco il numero multiplo di 3 e di 5 con FizzBuzz.
-        myClassAdd = "yellow";
+        myClassAdd = "yellow"; // coloro di giallo il quadrato FizzBuzz.
     } else {
         if (i % 3 === 0) {
             number = "Fizz"; //Sostituisco il numero multiplo di 3 con Fizz.
-            myClassAdd = "green";
+            myClassAdd = "green"; // coloro di verde il quadrato Fizz.
         } else if (i % 5 === 0) {
             number = "Buzz"; //Sostituisco il numero multiplo di 5 con Buzz.
-            myClassAdd = "red";
+            myClassAdd = "red"; // coloro di rosso il quadrato Buzz.
         }
     }
-    const squareBox = document.createElement("div"); // object
-    squareBox.innerHTML = number;
-    squareBox.classList.add(myClassAdd);
-    squareBox.classList.add("square");
-    square.append(squareBox);
+    const square = document.createElement("div"); // object
+    square.innerHTML = number;
+    square.classList.add(myClassAdd);
+    square.classList.add("square");
+    container.append(square);
     console.log(number);
 }
 
