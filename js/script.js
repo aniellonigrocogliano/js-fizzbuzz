481d9cb0b319ce69cf65bf247c4c18ab2f4268f7
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-03-27 14:59:46 +0100

    add: sostituzione dei numeri multipli di 3 con fizz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-for (let i = 0; i <=100; i++) { //creazione di 100 numeri da 1 a 100.
-    console.log(i);
+let number = -1;
+for (let i = 0; i <= 100; i++) { //creazione di 100 numeri da 1 a 100.
+    number = i;
     
+    if (i % 3 === 0) {
+        number = "Fizz";
+    }
+    console.log(number);
 }
